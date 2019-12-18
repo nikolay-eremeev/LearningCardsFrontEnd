e158dc1c6a168e76a3e28ec98489efac41c5ca6e
nikolay.eremeev <dbhbpjptv@gmail.com>
2019-12-18 14:35:10 +0100

    feature - using tcp socket as a frontend app

--- app/src/main/java/com/example/learningcards/LaunchActivity.java
+++ app/src/main/java/com/example/learningcards/LaunchActivity.java
@@ -4,22 +4,39 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import static android.speech.tts.TextToSpeech.QUEUE_ADD;
 
 public class LaunchActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = LaunchActivity.class.getSimpleName();
     static private ILearningCards myLearningCards;
     private TextView resultTextView;
     private TextView rememberTextView;
     private EditText userInputEditText;
+    private Switch ttsSwitch;
     private TextToSpeech myTTS;
+    boolean isTtsOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,91 +46,142 @@ public class LaunchActivity extends AppCompatActivity implements View.OnClickLis
         myTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                // TODO Auto-generated method stub
                 if (status == TextToSpeech.SUCCESS) {
                     int result = myTTS.setLanguage(Locale.US);
                     if (result == TextToSpeech.LANG_MISSING_DATA ||
                             result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Log.e("error", "This Language is not supported");
+                        Log.e(TAG, "This Language is not supported");
                     } else {
 
                     }
                 } else
-                    Log.e("error", "Initilization Failed!");
+                    Log.e(TAG, "Initilization Failed!");
             }
         });
+
         myTTS.setLanguage(Locale.ENGLISH);
 
         resultTextView = findViewById(R.id.textViewResult);
         rememberTextView = findViewById(R.id.textViewRemember);
         userInputEditText = findViewById(R.id.editTextUserInput);
+        ttsSwitch = findViewById(R.id.switch1);
         findViewById(R.id.buttonClean).setOnClickListener(this);
         findViewById(R.id.buttonNext).setOnClickListener(this);
+        ttsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    isTtsOn = true;
+                } else {
+                    isTtsOn = false;
+                }
+            }
+        });
 
-        if (myLearningCards == null)
-            myLearningCards = new LearningCards();
+        new Thread() {
+            public void run() {
+                if (myLearningCards == null) {
 
-        userInputEditText.setHint(myLearningCards.getWord());
+                    Writer writer = new StringWriter();
+                    char[] buffer = new char[1024];
+                    int n;
+
+                    try {
+                        InputStream is = getResources().openRawResource(R.raw.credentials);
+                        Reader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+                        while ((n = reader.read(buffer)) != -1) {
+                            writer.write(buffer, 0, n);
+                        }
+                        is.close();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    JSONObject obj;
+                    try {
+                        obj = new JSONObject(writer.toString());
+                        String host = obj.getString("hostname");
+                        int port = obj.getInt("port");
+                        myLearningCards = new LearningCards(host, port);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                userInputEditText.setHint(myLearningCards.getWord());
+
+            }
+        }.start();
     }
-
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonNext:
-                String userText = userInputEditText.getText().toString();
-                userInputEditText.getText().clear();
 
-                int resultColor;
-                String resultTitle;
+                if (myLearningCards != null) {
+                    String userText = userInputEditText.getText().toString();
+                    userInputEditText.getText().clear();
 
-                if (myLearningCards.getTranslationLanguage().equals("English"))
-                    myTTS.setLanguage(Locale.US);
-                else
-                    myTTS.setLanguage(new Locale("ru"));
+                    int resultColor;
+                    String resultTitle;
 
-                myTTS.speak(myLearningCards.getTranslation(), QUEUE_ADD, null, "id");
+                    if (isTtsOn) {
 
-                if (myLearningCards.getWordLanguage().equals("English"))
-                    myTTS.setLanguage(Locale.US);
-                else
-                    myTTS.setLanguage(new Locale("ru"));
 
-                myTTS.speak(myLearningCards.getWord(), QUEUE_ADD, null, "id");
+                        if (myLearningCards.getTranslationLanguage().equals("English"))
+                            myTTS.setLanguage(Locale.US);
+                        else if (myLearningCards.getTranslationLanguage().equals("Russian"))
+                            myTTS.setLanguage(new Locale("ru"));
 
-                rememberTextView.setText(myLearningCards.getWord() + "\n\n" + myLearningCards.getTranslation() + "\n\n" + myLearningCards.getScore());
+                        myTTS.speak(myLearningCards.getTranslation(), QUEUE_ADD, null, "id");
 
-                boolean isAnswerRight = myLearningCards.pushAnswer(userText);
+                        if (myLearningCards.getWordLanguage().equals("English"))
+                            myTTS.setLanguage(Locale.US);
+                        else if (myLearningCards.getWordLanguage().equals("Russian"))
+                            myTTS.setLanguage(new Locale("ru"));
 
-                if (isAnswerRight) {
-                    resultColor = getResources().getColor(R.color.colorPrimary);
-                    resultTitle = getResources().getString(R.string.title_right_answer);
-                } else {
-                    resultColor = getResources().getColor(R.color.colorAccent);
-                    resultTitle = getResources().getString(R.string.title_wrong_answer);
-                }
-
-                resultTextView.setText(resultTitle);
-                resultTextView.setTextColor(resultColor);
-                rememberTextView.setTextColor(resultColor);
-
-                new Thread() {
-                    public void run() {
-                        try {
-                            Thread.sleep(1000);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    resultTextView.setText("");
-                                }
-                            });
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                        myTTS.speak(myLearningCards.getWord(), QUEUE_ADD, null, "id");
                     }
-                }.start();
+                    rememberTextView.setText(myLearningCards.getWord() + "\n\n" + myLearningCards.getTranslation() + "\n\n" + myLearningCards.getScore());
 
-                userInputEditText.setHint(myLearningCards.getWord());
+                    boolean isAnswerRight = myLearningCards.pushAnswer(userText);
+
+                    if (isAnswerRight) {
+                        resultColor = getResources().getColor(R.color.colorPrimary);
+                        resultTitle = getResources().getString(R.string.title_right_answer);
+                    } else {
+                        resultColor = getResources().getColor(R.color.colorAccent);
+                        resultTitle = getResources().getString(R.string.title_wrong_answer);
+                    }
+
+                    resultTextView.setText(resultTitle);
+                    resultTextView.setTextColor(resultColor);
+                    rememberTextView.setTextColor(resultColor);
+
+                    new Thread() {
+                        public void run() {
+                            try {
+                                Thread.sleep(1000);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        resultTextView.setText("");
+                                    }
+                                });
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }.start();
+
+                    userInputEditText.setHint(myLearningCards.getWord());
+                } else {
+                    Log.e(TAG, "LearningCards are not initialized");
+                }
                 break;
 
             case R.id.buttonClean:
@@ -126,8 +194,16 @@ public class LaunchActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onStop() {
         super.onStop();
-        //TODO save user's LearningCardsImpl
     }
 
+    @Override
+    protected void onDestroy() {
+        if (myTTS != null) {
+            myTTS.stop();
+            myTTS.shutdown();
+            Log.d(TAG, "TTS Destroyed");
+        }
+        super.onDestroy();
+    }
 
 }
--- app/src/main/java/com/example/learningcards/LearningCards.java
+++ app/src/main/java/com/example/learningcards/LearningCards.java
@@ -1,45 +1,46 @@
 package com.example.learningcards;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Random;
 
 class LearningCards implements ILearningCards {
-    private static final String TAG = "LearningCards";
+    private static final String TAG = LearningCards.class.getSimpleName();
     private ArrayList<Card> cardsArrayList = new ArrayList<>();
     private Card activeCard;
     private int activeCardNumber;
     private int activeWordNumber;
 
-    LearningCards() {
+    LearningCards(String hostname, int port) {
+        getData(hostname, port);
+        this.pickWordToLearn();
+    }
 
-        String[] input = ("English\tRussian\thurricane damage poses a major threat to many coastal communities\tущерб от урагана представляет серьезную угрозу для многих прибрежных сообществ\n" +
-                "English\tRussian\tthe reports of the commission are often referred to in the media\tна сообщения комиссии часто ссылаются в средствах массовой информации\n" +
-                "English\tRussian\tthe sport gives an enormous amount of pleasure to many people\tспорт доставляет огромное количество удовольствия многим людям\n" +
-                "English\tRussian\the managed to spill a little lake of alcohol on the bookshelf\tему удалось пролить небольшое озеро алкоголя на книжную полку\n" +
-                "English\tRussian\twe look forward to a very rewarding relationship with them\tмы с нетерпением ждем очень полезных отношений с ними\n" +
-                "English\tRussian\tit is a puzzle that any intelligent child could solve\tэто головоломка, которую может решить любой умный ребенок\n" +
-                "English\tRussian\the was driving in a manner likely to endanger life\tон ехал в манере, способной поставить под угрозу жизнь\n" +
-                "English\tRussian\the conducts research in the field of nuclear physics\tон проводит исследования в области ядерной физики\n" +
-                "English\tRussian\tthe kids are mad keen on computer games at the moment\tдети без ума от компьютерных игр в данный момент\n" +
-                "English\tRussian\texistence is about to be threatened by a strange new blight\tсуществованию грозит новый странный упадок\n" +
-                "English\tRussian\tthese exercises are good for building up leg strength\tэти упражнения полезны для наращивания силы ног\n" +
-                "English\tRussian\twhen they grow up they will be able to swim like us\tкогда они вырастут, они смогут плавать, как мы\n" +
-                "English\tRussian\tshe realized that, he did not seem at all upset\tона поняла, что он, похоже, совсем не расстроился\n" +
-                "English\tRussian\tthousands of people bet on the result of the match\tтысячи людей сделали ставку на результат матча\n" +
-                "English\tRussian\tthe handwriting on the letter was neat and feminine\tпочерк на письме был аккуратным и женственным\n" +
-                "English\tRussian\tthe conditions for cooperation were clearly defined\tусловия сотрудничества были четко определены\n" +
-                "English\tRussian\tthe government is keen to encourage investment\tправительство стремится стимулировать инвестиции\n" +
-                "English\tRussian\twe collect data on the state of the environment\tмы собираем данные о состоянии окружающей среды\n" +
-                "English\tRussian\tvegetables should be stored in a cool dry place\tовощи следует хранить в сухом прохладном месте\n" +
-                "English\tRussian\tThe man hesitated, his eyes flicking nervously\tМужчина колебался, его глаза нервно щурелились\n" +
-                "English\tRussian\tthis has been a quite fantastically rewarding experience\tэто был довольно фантастический опыт\n" +
-                "English\tRussian\tan example of a shuffle is a sliding step in a dance\tпример шаффла - скользящий шаг в танце").split("\n");
+    void getData(String hostname, int port) {
+        String[] inputArr;
+        try (Socket socket = new Socket(hostname, port)) {
+            InputStream input = socket.getInputStream();
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(input));
+            String inputString = buffer.readLine();
+            inputArr = inputString.split(":::");
+            buffer.close();
+            for (String str : inputArr) {
+                cardsArrayList.add(new Card(str.split("\t")));
+            }
 
-        for (String str : input) {
-            cardsArrayList.add(new Card(str.split("\t")));
+        } catch (UnknownHostException ex) {
+            Log.e(TAG, "Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            Log.e(TAG,"I/O error: " + ex.getMessage());
         }
 
-        this.pickWordToLearn();
     }
 
     private void pickWordToLearn() {
