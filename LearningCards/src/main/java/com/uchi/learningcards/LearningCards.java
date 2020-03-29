97acd32983d534553be6e5f2f5134932d963d563
nikolay.eremeev <dbhbpjptv@gmail.com>
2020-03-29 18:58:48 +0200

    test mode added

--- LearningCards/src/main/java/com/uchi/learningcards/LearningCards.java
+++ LearningCards/src/main/java/com/uchi/learningcards/LearningCards.java
@@ -20,25 +20,31 @@ class LearningCards {
     private int activeCardNumber;
 
     LearningCards(String hostname, int port) throws IOException {
-        getData(hostname, port);
+        getData(hostname, port, false);
         this.pickWordToLearn();
     }
 
-    private void getData(String hostname, int port) throws IOException {
-        String[] inputArr;
+    private void getData(String hostname, int port, boolean test) throws IOException {
 
+        String[] inputArr;
+        if (test) {
+            String inputString = "English\tRussian\tdot\tточка\tпятнышко:::English\tRussian\tfog\tтуман\tтуманный:::English\tRussian\tharm\tпричинять вред\tущерб:::English\tRussian\tpile\tкуча\tгруда:::English\tRussian\tpin\tштырь\tштифт:::English\tRussian\trib\tребро:::English\tRussian\triot\tбунт\tбеспорядки:::English\tRussian\tscent\tзапах\tнюх:::English\tRussian\twhip\tкнут\tхлыст:::English\tRussian\tfund\tфонд\tфинансировать:::English\tRussian\ttoward\tк\tв направлении:::English\tRussian\tneck\tшея:::English\tRussian\tbeam\tлуч\tбалка:::English\tRussian\tendure\tтерпеть\tвынести:::English\tRussian\tbow\tлук\tпоклон:::English\tRussian\tcue\tкий\tсигнал:::English\tRussian\tpit\tяма\tкарьер:::English\tRussian\tblow\tудар\tдуть:::English\tRussian\tbay\tзалив\tбухта:::English\tRussian\tcab\tтакси:::English\tRussian\tcord\tшнур\tверевка:::English\tRussian\tdam\tплотина\tдамба";
+            inputArr = inputString.split(":::");
+        } else {
             InetSocketAddress endPoint = new InetSocketAddress(hostname, port);
             Socket socket = new Socket();
             socket.connect(endPoint, 10000);
-
             InputStream input = socket.getInputStream();
             BufferedReader buffer = new BufferedReader(new InputStreamReader(input));
             String inputString = buffer.readLine();
             inputArr = inputString.split(":::");
             buffer.close();
-            for (String str : inputArr) {
-                cardsArrayList.add(new Card(str.split("\t")));
-            }
+        }
+
+        for (String str : inputArr) {
+            cardsArrayList.add(new Card(str.split("\t")));
+        }
+
     }
 
     private void pickWordToLearn() {
